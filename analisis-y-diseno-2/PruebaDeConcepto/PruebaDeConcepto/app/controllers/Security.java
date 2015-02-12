51694050a8ef85041c9c7010d1ba1939b7a1d267
ray.isistemas@gmail.com <ray.isistemas@gmail.com@768141e2-8b69-4312-4ac3-f6ad0a3a5975>
2015-02-12 00:52:21 +0000

    Fase 1 calificaion

--- analisis-y-diseno-2/PruebaDeConcepto/PruebaDeConcepto/app/controllers/Security.java
+++ analisis-y-diseno-2/PruebaDeConcepto/PruebaDeConcepto/app/controllers/Security.java
@@ -1,5 +1,8 @@
 package controllers;
 
+import java.util.List;
+
+import models.Usuario;
 import play.mvc.Controller;
 
 public class Security extends Controller {
@@ -10,11 +13,28 @@ public class Security extends Controller {
 	
 	public static void authenticate(String email, String password) {
 		
+		System.out.println("hola");
 		System.out.println(email);
 		System.out.println(password);
 		
 		session.put("nombre", email);
-		Application.index();
+		
+		List<Usuario> usuarios = Usuario.find("email = ? and password=?",email,password).fetch();
+		
+		Usuarios esto deberia fallar
+		
+		if (usuarios.isEmpty()) {
+			System.out.println("ERRROR");
+			login();
+		} else {
+			Application.index();
+		}
+			
+//		Usuario usuario = new Usuario();
+//		usuario.email ="patito@algo.com";
+//		usuario.edad = 15;
+//		usuario.nombre = "Patito";
+//		usuario.save();
 		
 	}
 }
